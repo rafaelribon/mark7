449fdf5a7322a4649f5e57002e28e9446247dac0
“rafaelribon <ribon.rafael@gmail.com>
2018-06-13 18:50:11 -0300

    Capturando as Evidencias

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,8 +2,23 @@ require 'capybara'
 require 'capybara/cucumber'
 require 'site_prism'
 
+@env = ENV['BROWSER']
+
 Capybara.configure do |config|
-    config.default_driver = :selenium
+
+# if @env.eql?('firefox')
+#     config.default_driver = :selenium
+# elsif @env.eql?('chrome')
+#     config.default_driver = :selenium_chrome
+# elsif @nev.eql?('headless')
+#     config.default_driver = :selenium_chrome_headless
+# end
+
+    #testar no firefox 
+    #config.default_driver = :selenium
+    #testar no chrome 
+    #config.default_driver = :selenium_chrome
+    config.default_driver = :selenium_chrome_headless
     config.app_host = 'https://mark7.herokuapp.com'
 end 
 
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -16,3 +16,17 @@ After('@logout') do
     @nav.menu_usuario.click
     @nav.logout.click
 end
+
+# tira print da evidencia 
+After  do |scenario|
+    #recebe o nome do cénario
+    #troca os espaço por underscore  
+    #converte maisculo em minusculo 
+    nome_cenario = scenario.name.tr(' ','_').downcase!   
+    #local onde vai gravar a evidencia
+    # nome do cenario.png
+    evidencia = "log/screenshots/#{nome_cenario}.png"
+    #puts evidencia  #exibe o nome das evidencias para verificar funcionamento 
+    page.save_screenshot(evidencia)
+    embed(evidencia,'image/png','Clique aqui para ver a evidencia!')
+end 
